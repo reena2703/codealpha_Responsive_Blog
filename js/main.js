48ffac2996355021f90ed1e70691b8a1c9c9cf17
reena <109857180+reena2703@users.noreply.github.com>
2023-09-24 23:01:36 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Filter Js
+$(document).ready(function())
+$(".filter-item").click(function(){
+    const value =$(this).attr('data-filter')
+    if(value =='all'){
+        $('.post-box').show('1000')
+    } 
+    else{
+      $(".post-box")
+      .not('.' + value)
+      .hide("1000");
+      $(".post-box")
+      .filter('.' + value)
+      .show("1000");
+    }
+});
+// Add active to btn
+$(".filter-item").click(function(){
+  $(this).addclass("active-filter").sibling().removeclass("active-filter");
+});
+// Header BackGround Change On Scroll
+let header = document.querySelector('header');
+window.addEventListener('scroll',()=>{
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
